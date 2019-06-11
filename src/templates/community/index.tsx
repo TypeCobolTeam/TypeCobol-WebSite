92a45e7ba79a943a160bbb2c3b32ba5460a2d459
STRENG Nicolas (STAGIAIRE) <nicolas@nicolasstr.eu>
2019-06-11 15:49:55 +0200

    WI #19 Create basic templates

---
+++ src/templates/community/index.tsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+import { graphql } from "gatsby";
+
+interface pageProps {
+  data: {
+    site: {
+      siteMetadata: {
+        title: string;
+      };
+    };
+  };
+}
+
+export const pageQuery = graphql`
+  query CommunityMarkdown {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`;
+
+class TutorialTemplate extends React.Component<pageProps, {}> {
+  readonly hello = `Hello`;
+  public render() {
+    const { title } = this.props.data.site.siteMetadata;
+    return (
+      <div>
+        <h1>{this.hello} TypeScript world!</h1>
+        <p>
+          This site is named <strong>{title}</strong>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default TutorialTemplate;
