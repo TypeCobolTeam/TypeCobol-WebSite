9f53e036a128fe0accf60ece29c82668381868cc
Nicolas Streng <nicolas@nicolasstr.eu>
2019-07-04 16:52:54 +0200

    WI #45 Add Image Import In Md-Files (#46)

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,29 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://xcmci02d.formation.cm-cic.fr`,
-    title: `TypeCobol`,
-    desc: `An Incremental Cobol parser for IBM Enterprise Cobol 5.1 zOS syntax. TypeCobol is also an extension of Cobol 85 language which can then be converted to Cobol85.`,
+    siteUrl: "https://xcmci02d.formation.cm-cic.fr",
+    title: "TypeCobol",
+    desc:
+      "An Incremental Cobol parser for IBM Enterprise Cobol 5.1 zOS syntax. TypeCobol is also an extension of Cobol 85 language which can then be converted to Cobol85.",
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sharp",
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/content/`,
         name: "content",
       },
     },
     {
-      resolve: `gatsby-transformer-remark`,
+      resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
           {
-            resolve: `gatsby-remark-codemirror`,
+            resolve: "gatsby-remark-images",
+          },
+          {
+            resolve: "gatsby-remark-codemirror",
             options: {
               theme: "typecobol",
             },
@@ -27,7 +32,7 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-breadcrumb`,
+      resolve: "gatsby-plugin-breadcrumb",
       options: {
         defaultCrumb: {
           location: {
@@ -39,26 +44,26 @@ module.exports = {
         },
       },
     },
-    `gatsby-plugin-sitemap`,
-    `gatsby-plugin-catch-links`,
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-catch-links",
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: "gatsby-plugin-manifest",
       options: {
-        name: `TypeCobol`,
-        short_name: `TypeCobol`,
-        start_url: `/`,
-        background_color: `#f0f2f5`,
-        theme_color: `#ffffff`,
-        display: `standalone`,
+        name: "TypeCobol",
+        short_name: "TypeCobol",
+        start_url: "/",
+        background_color: "#f0f2f5",
+        theme_color: "#ffffff",
+        display: "standalone",
         icon: "content/images/favicon.png",
       },
     },
     {
-      resolve: `gatsby-plugin-offline`,
+      resolve: "gatsby-plugin-offline",
       options: {
-        cacheId: `typecobol-cache`,
+        cacheId: "typecobol-cache",
       },
     },
-    `gatsby-plugin-meta-redirect`,
+    "gatsby-plugin-meta-redirect",
   ],
 }
